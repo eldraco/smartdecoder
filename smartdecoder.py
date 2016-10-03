c32d1ff319bd44716eef128dc4621a9f49f65a8b
eldraco <eldraco@gmail.com>
2016-10-03 21:22:16 +0200

    first version

---
+++ smartdecoder.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# Fast hack to convert a string containing ascii, hexa and unicode all mixed to ascii.
+# Author: Sebastian Garcia, eldraco@gmail.com
+import sys
+
+# example string "\x64\u0065\u006cay\u0042\x65\u0074\u0077\u0065e\x6e"
+#a = sys.stdin.readline()
+#a = a.strip()
+a = raw_input()
+print a
+print type(a)
+#a = "\x64\u0065\u006cay\u0042\x65\u0074\u0077\u0065e\x6e"
+
+n = ''
+i=0
+while i < len(a):
+    if a[i] == '\\':
+        # unicode or hexa
+        if a[i+1] == 'x':
+            temp = a[i+2:i+4].decode("hex")
+            print '{}: hexa'.format(temp)
+            n += temp
+            i += 4
+        elif a[i+1] == 'u':
+            # unicode
+            temp = unicode(a[i:i+6])
+            print '{}: uni'.format(temp.decode('unicode-escape'))
+            n += temp.decode('unicode-escape')
+            i += 5
+    else:
+        print '{}: ascii'.format(a[i])
+        n += a[i]
+        i+=1
+print n
