ea05047a02ed7fb119970e9ecbdf8cf2a520b831
eldraco <eldraco@gmail.com>
2016-10-03 21:25:28 +0200

    better

--- smartdecoder.py
+++ smartdecoder.py
@@ -4,14 +4,8 @@
 import sys
 
 # example string "\x64\u0065\u006cay\u0042\x65\u0074\u0077\u0065e\x6e"
-#a = sys.stdin.readline()
-#a = a.strip()
-a = raw_input()
-print a
-print type(a)
-#a = "\x64\u0065\u006cay\u0042\x65\u0074\u0077\u0065e\x6e"
-# "delayBetween"
-
+a = sys.stdin.readline()
+a = a.strip()
 n = ''
 i=0
 while i < len(a):
@@ -19,18 +13,17 @@ while i < len(a):
         # unicode or hexa
         if a[i+1] == 'x':
             temp = a[i+2:i+4].decode("hex")
-            print '{}: hexa'.format(temp)
+            #print '{}: hexa'.format(temp)
             n += temp
             i += 4
         elif a[i+1] == 'u':
             # unicode
-            #temp = unicode(a[i:i+6])
             temp = unicode(a[i:i+6])
-            print '{}: uni'.format(temp.decode('unicode-escape'))
+            #print '{}: uni'.format(temp.decode('unicode-escape'))
             n += temp.decode('unicode-escape')
             i += 6
     else:
-        print '{}: ascii'.format(a[i])
+        #print '{}: ascii'.format(a[i])
         n += a[i]
         i+=1
 print n
